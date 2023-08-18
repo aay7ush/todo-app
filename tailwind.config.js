44e9b4ada600459d87388e3501269b1c95f72bcf
Aayush Jaiswal <aay7ush@gmail.com>
2023-08-18 20:53:39 +0530

    fixed minor bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Josefin Sans', 'sans-serif'],
+        sans: ["Josefin Sans", "sans-serif"],
       },
       colors: {
-        'clr-bg-main': 'var(--clr-bg-main)',
-        'clr-bg-input': 'var(--clr-bg-input)',
-        'clr-bg-border': 'var(--clr-bg-border)',
-        'clr-text-primary': 'var(--clr-text-primary)',
-        'clr-text-secondary': 'var(--clr-text-secondary)',
-        'clr-accent': 'var(--clr-accent)',
-        'clr-line-through': 'var(--clr-line-through)',
-        'clr-gradient-1': 'var(--clr-gradient-1)',
-        'clr-gradient-2': 'var(--clr-gradient-2)',
+        "clr-bg-main": "var(--clr-bg-main)",
+        "clr-bg-input": "var(--clr-bg-input)",
+        "clr-bg-border": "var(--clr-bg-border)",
+        "clr-text-primary": "var(--clr-text-primary)",
+        "clr-text-secondary": "var(--clr-text-secondary)",
+        "clr-accent": "var(--clr-accent)",
+        "clr-line-through": "var(--clr-line-through)",
+        "clr-gradient-1": "var(--clr-gradient-1)",
+        "clr-gradient-2": "var(--clr-gradient-2)",
       },
     },
   },
